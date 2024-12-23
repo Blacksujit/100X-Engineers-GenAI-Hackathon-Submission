a889a22a24ebb4a0c7b9d8bc89c0cfbc197ea9ba
black shadow <nirmalsujit541@gmail.com>
2024-12-24 03:10:49 +0530

    New code bloacks  added in the file ✅

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_caching import Cache
 def create_app():
-    app = Flask(__name__, template_folder='../templates')  # Specify the path to the templates folder
+    app = Flask(__name__, template_folder='../templates', static_folder='static' )  # Specify the path to the templates folder
     # Set cache directory and other caching parameters
     app.config['CACHE_TYPE'] = 'filesystem'
     app.config['CACHE_DIR'] = 'F:\\100x_enginners_hackathon_genai\\app\\__pycache__'  # Change this to your desired cache path
