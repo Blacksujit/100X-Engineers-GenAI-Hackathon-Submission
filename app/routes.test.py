9eca02acd303223764da6ae0025d9ffecc5538b5
black shadow <nirmalsujit541@gmail.com>
2024-12-24 02:51:37 +0530

    New code bloacks  added in the file ✅

--- app/routes.test.py
+++ app/routes.test.py
@@ -12,3 +12,9 @@ def test_index_route(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'<!DOCTYPE html>' in response.data
+    
+    
+    # this is the sample test case for the routes  code 
+    
+    
+    
