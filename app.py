97cc2a0b716560bfd3c8990bbe4ba0a1f17dfa8f
black shadow <nirmalsujit541@gmail.com>
2024-12-27 02:45:18 +0530

    Errors are there in csv to video moddule will do tommorow ✅

---
+++ app.py
@@ -0,0 +1,16 @@
+from app import create_app
+import os
+import logging
+
+app = create_app()
+
+if __name__ == "__main__":
+    try:
+        os.makedirs("uploads", exist_ok=True)
+<<<<<<< HEAD
+=======
+        os.makedirs("models", exist_ok=True)
+>>>>>>> 795a1e6 ( Errors are there in csv to video moddule will do tommorow ✅)
+        app.run(debug=True, port=200)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
